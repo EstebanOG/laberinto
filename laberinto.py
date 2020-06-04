a44ab60fd91ccd8bcfa0b8be3b95332f4f405938
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2020-06-04 13:24:28 -0500

    Cambios en la forma en que se fraccionan las líneas

--- laberinto.py
+++ laberinto.py
@@ -1,3 +1,3 @@
 def abre_archivo():
-	return [(elemento)for x in open("matriz.txt","r").readlines() for elemento in x if(elemento!='\n' and elemento!=' ')]
+	return [[elemento for elemento in x if(elemento!='\n' and elemento!=' ')]for x in open("matriz.txt","r").readlines() ]
 print(abre_archivo())
