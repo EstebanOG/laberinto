dfe2073404b9fbda0216469dc5ecfa79ca917c24
Esteban <juanesteban199911@gmail.com>
2020-06-04 13:49:53 -0500

    Funcion busqueda X

---
+++ laberinto.py
@@ -0,0 +1,11 @@
+matriz = [[1,2,3,4],[1,2,'X',3],[1,2,3,10]]
+
+def buscar_en_matriz(matriz,cont):
+    if matriz == []:
+        return (-1,-1)
+    if "X" in matriz[0]: 
+        return (cont,matriz[0].index("X"))
+    return buscar_en_matriz(matriz[1:],cont+1)
+
+print(buscar_en_matriz(matriz,0))
+
