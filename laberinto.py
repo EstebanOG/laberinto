4c9cdf20352b68bc48cd8e3e92b06e75e517421e
Esteban <juanesteban199911@gmail.com>
2020-06-08 08:44:30 -0500

    Cambios

--- laberinto.py
+++ laberinto.py
@@ -8,6 +8,13 @@ def abre_archivo2(archivo):
 
 #print(abre_archivo2())
 '''
+def buscar_en_matriz(matriz,cont):
+    if matriz == []:
+        return (-1,-1)
+    if "X" in matriz[0]: 
+        return (cont,matriz[0].index("X"))
+    return buscar_en_matriz(matriz[1:],cont+1)
+
 def abre_archivo(archivo):
 	return [[elemento for elemento in x if(elemento!='\n' and elemento!=' ')]for x in open(archivo,"r").readlines() ]
 
