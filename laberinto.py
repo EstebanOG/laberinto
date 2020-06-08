fe3aedfde6f83a006156a91df20bed9998b22b3a
Esteban <juanesteban199911@gmail.com>
2020-06-08 08:49:26 -0500

    Se une busqueda con archivo

--- laberinto.py
+++ laberinto.py
@@ -1,13 +1,3 @@
-'''
-def abre_archivo(archivo):
-	return [x for x in open(archivo,"r").readlines()]
-
-
-def abre_archivo2(archivo):
-	return [(elemento)for x in open(archivo,"r").readlines() for elemento in x if(elemento!='\n' and elemento!=' ')]
-
-#print(abre_archivo2())
-'''
 def buscar_en_matriz(matriz,cont):
     if matriz == []:
         return (-1,-1)
@@ -15,9 +5,9 @@ def buscar_en_matriz(matriz,cont):
         return (cont,matriz[0].index("X"))
     return buscar_en_matriz(matriz[1:],cont+1)
 
-def abre_archivo(archivo):
-	return [[elemento for elemento in x if(elemento!='\n' and elemento!=' ')]for x in open(archivo,"r").readlines() ]
+def abre_archivo():
+	return [[elemento for elemento in x if(elemento!='\n' and elemento!=' ')]for x in open("matriz.txt","r").readlines() ]
 
-#print(abre_archivo())
+print(buscar_en_matriz(abre_archivo(),0))
 
 
