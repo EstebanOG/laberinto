abe2db4b0a5ec03611517f7e4d4f1d837a77dfd3
Esteban <juanesteban199911@gmail.com>
2020-06-09 12:04:14 -0500

    Se recorre laberinto

--- laberinto.py
+++ laberinto.py
@@ -7,49 +7,51 @@ def buscar_en_matriz(matriz,cont):
 
 def abre_archivo():
 	return [[elemento for elemento in x if(elemento!='\n' and elemento!=' ')]for x in open("matriz.txt","r").readlines() ]
-def recorrer(mapa, jugador):
-    print(mapa)
+def recorrer(mapa, jugador,camino):
+    #print(mapa)
+    #print(jugador)
+    #print(camino)
     if jugador[1] != len(mapa[0])-1 and mapa[jugador[0]][jugador[1]+1] == "Y":
         mapa[jugador[0]][jugador[1]] = "0"
         mapa[jugador[0]][jugador[1]+1] = "Y"
         jugador[1] = jugador[1]+1
-        return jugador
+        return camino+jugador
     elif mapa[jugador[0]-1][jugador[1]] == "Y":
         mapa[jugador[0]][jugador[1]] = "0"
         mapa[jugador[0]-1][jugador[1]] = "Y"
         jugador[0] = jugador[0]-1
-        return jugador
+        return camino+jugador
     elif mapa[jugador[0]][jugador[1]-1] == "Y":
         mapa[jugador[0]][jugador[1]] = "0"
         mapa[jugador[0]][jugador[1]-1] = "Y"
         jugador[1] = jugador[0]-1
-        return jugador
+        return camino+jugador
     elif mapa[jugador[0]][jugador[1]] == "Y":
         mapa[jugador[0]][jugador[1]] = "0"
         mapa[jugador[0]+1][jugador[1]] = "Y"
         jugador[0] = jugador[0]+1
-        return jugador
-    elif jugador[1] != len(mapa[0])-1 and mapa[jugador[0]][jugador[1]+1] == "0":
-        mapa[jugador[0]][jugador[1]] = "0"
+        return camino+jugador
+    elif jugador[1] < len(mapa[0])-1 and mapa[jugador[0]][jugador[1]+1] == "0" and jugador[1]+1 < len(mapa[0])-1:
+        mapa[jugador[0]][jugador[1]] = "-"
         mapa[jugador[0]][jugador[1]+1] = "X"
         jugador[1] = jugador[1]+1
-        return recorrer(mapa, jugador)
+        return recorrer(mapa, jugador, camino+jugador)
     elif mapa[jugador[0]-1][jugador[1]] == "0":
-        mapa[jugador[0]][jugador[1]] = "0"
+        mapa[jugador[0]][jugador[1]] = "-"
         mapa[jugador[0]-1][jugador[1]] = "X"
         jugador[0] = jugador[0]-1
-        return recorrer(mapa, jugador)
+        return recorrer(mapa, jugador, camino+jugador)
     elif jugador[1] != 0 and mapa[jugador[0]][jugador[1]-1] == "0":
-        mapa[jugador[0]][jugador[1]] = "0"
+        mapa[jugador[0]][jugador[1]] = "-"
         mapa[jugador[0]][jugador[1]-1] = "X"
-        jugador[1] = jugador[0]-1
-        return recorrer(mapa, jugador)
-    elif mapa[jugador[0]][jugador[1]] == "0":
-        mapa[jugador[0]][jugador[1]] = "0"
+        jugador[1] = jugador[1]-1
+        return recorrer(mapa, jugador, camino+jugador)
+    elif mapa[jugador[0]+1][jugador[1]] == "0":
+        mapa[jugador[0]][jugador[1]] = "-"
         mapa[jugador[0]+1][jugador[1]] = "X"
         jugador[0] = jugador[0]+1
-        return recorrer(mapa, jugador)
+        return recorrer(mapa, jugador, camino+jugador)
     
+print(recorrer(abre_archivo(),buscar_en_matriz(abre_archivo(),0),[]))
 
-print(recorrer(abre_archivo(),buscar_en_matriz(abre_archivo(),0)))
 
